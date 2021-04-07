e1a69ca8734c5c8a1cdecfb05742dddfe84aa1a6
aku <61090626+akucchi@users.noreply.github.com>
2021-04-08 01:59:41 +0200

    :lipstick: Added style to ProjectContents

--- src/components/ProjectContents.tsx
+++ src/components/ProjectContents.tsx
@@ -1,19 +1,52 @@
-import React from "react";
-import {Cinematic, Project, Replay} from "../interfaces/project"
+import React from 'react';
+import styled from 'styled-components';
+import { useRocketLeague } from '../hooks/bakkesmod';
+import { Cinematic, Replay } from '../interfaces/project';
 
-export function ProjectContents({replays, cinematics}: any) {
-  return(
-    <>
+const Main = styled.div`
+  margin: 4px;
+  height: calc(100% - 36px);
+  overflow-y: scroll;
+  overflow-x: hidden;
+  &:hover {
+    ::-webkit-scrollbar-thumb {
+      background: #babbc0;
+    }
+  }
+  h3 {
+    margin: 0;
+  }
+`;
+
+const Item = styled.div`
+  padding: 4px;
+  margin-right: 4px;
+  border-bottom: 1px solid #babbc0;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export function ProjectContents({ replays, cinematics, onChange }: any) {
+  const { loadReplay } = useRocketLeague();
+
+  return (
+    <Main>
+      {replays.length ? <h3>Replays</h3> : null}
       {replays.map((replay: Replay) => (
-        <div key={replay.name}>
+        <Item
+          key={replay.name}
+          onClick={() => loadReplay('Park_P', replay.path)}
+        >
           {replay.name}
-        </div>
+        </Item>
       ))}
+      {cinematics.length ? <h3>Cinematics</h3> : null}
       {cinematics.map((cinematic: Cinematic) => (
-        <div key={cinematic.name}>
+        <Item key={cinematic.name} onClick={() => onChange(cinematic.name)}>
           {cinematic.name}
-        </div>
+        </Item>
       ))}
-
-    </>);
+    </Main>
+  );
 }
